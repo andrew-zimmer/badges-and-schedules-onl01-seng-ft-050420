dba92707b9631d7a0118330e91f2ce08f6c9ba0b
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-05 21:29:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 def batch_badge_creator(array)
   new_array = []
   array.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{array.length +1}!")
+    new_array.push("Hello, my name is #{name}.")
   end 
-  return new_array
+  new_array
 end

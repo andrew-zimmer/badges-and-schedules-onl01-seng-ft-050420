687585c5b9a81ab931a51328d0e913ca60d6408f
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-05 21:31:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(array)
   new_array = []
   array.each do |name|
-    new_array.push("Hello, #{name}! You'll be assigned to room #{array.length +1}!")
+    new_array.push("Hello, #{name}! You'll be assigned to room #{array.index(name) +1}!")
   end 
   return new_array
 end
